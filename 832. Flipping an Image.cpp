f71b8203083b181128676b362e4be977c60d131d
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-08-22 00:19:25 +0530

    Create 832. Flipping an Image.cpp

---
+++ 832. Flipping an Image.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        for(int i=0;i<image.size();i++){
+            for(int j=0;j<image[i].size()/2;j++){
+                int temp=image[i][j];
+                image[i][j]=image[i][image[i].size()-1-j];
+                image[i][image[i].size()-1-j]=temp;
+            }
+        }
+        for(int i=0;i<image.size();i++){
+            for(int j=0;j<image[i].size();j++){
+                if(image[i][j]==0){
+                    image[i][j]=1;
+                }else{
+                    image[i][j]=0;
+                }
+            }
+        }
+        return image;
+    }
+};
