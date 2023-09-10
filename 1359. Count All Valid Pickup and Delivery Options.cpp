db6db47ce2886660af73e4d7c7a050d8684719d1
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-09-10 11:57:06 +0530

    Create 1359. Count All Valid Pickup and Delivery Options.cpp

---
+++ 1359. Count All Valid Pickup and Delivery Options.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long mod=1000000007;
+        long res=1;
+        for(int i=1;i<=n;i++){
+            res*=i;
+            res%=mod;
+            res*=(2*i)-1;
+            res%=mod;
+        }
+        return res%mod;
+    }
+};
