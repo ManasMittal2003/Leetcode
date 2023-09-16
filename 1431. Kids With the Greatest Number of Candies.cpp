0849b7ffbeda189b42d100ffa728700ac2c1bd87
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-09-16 16:05:50 +0530

    Update 1431. Kids With the Greatest Number of Candies.cpp

--- 1431. Kids With the Greatest Number of Candies.cpp
+++ 1431. Kids With the Greatest Number of Candies.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        vector<int> ans(candies.size(),-1);
-        int flag=0;
-        vector<int>::iterator it;
+        vector<bool> ans;
+        vector<int> temp2;
+        int max=INT_MIN;
         for(int i=0;i<candies.size();i++){
-            if(ans[i]==-1){
-                int flag=0;
-                for(int j=0;j<candies.size();j++){
-                    if(candies[i]+extraCandies<candies[j]){
-                        flag=1;
-                        ans[i]=0;
-                        break;
-                    }
-                }
-                if(flag==0){
-                    ans[i]=1;
-                    it=find(candies.begin(),candies.end(),candies[i]);
-                    if(it!=candies.end()){
-                        ans[it-candies.begin()]=1;
-                    }
-                }
+            if(candies[i]>max){
+                max=candies[i];
             }
         }
-        vector<bool> ans2;
-        for(auto it:ans){
-            if(it==1){
-                ans2.push_back(true);
+        for(int i=0;i<candies.size();i++){
+            temp2.push_back(candies[i]+extraCandies);
+        }
+        for(auto i:temp2){
+            if(i>=max){
+                ans.push_back(true);
             }else{
-                ans2.push_back(false);
+                ans.push_back(false);
             }
         }
-        return ans2;
+        return ans;
     }
 };
