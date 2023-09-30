c96090c79522a22a0ad01b4d17d8937f24152367
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-09-30 19:41:33 +0530

    Update 1470. Shuffle the Array.cpp

--- 1470. Shuffle the Array.cpp
+++ 1470. Shuffle the Array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int temp=nums[nums.size()/2];
-        int j=0,k=nums.size()/2,a=1,s;
-        while(j<(nums.size()/2)-1){
-            for(s=k;s>a;s--){
-                nums[s]=nums[s-1];
+        vector<int> ans;
+        int low=0,mid=n;
+        for(int i=0;i<2*n;i++){
+            if(i%2==0){
+                ans.push_back(nums[low]);
+                low++;
+            }else{
+                ans.push_back(nums[mid]);
+                mid++;
             }
-            nums[s]=temp;
-            j++;
-            k++;
-            temp=nums[k];
-            a=a+2;
         }
-        return nums;
-   }
+        return ans;
+    }
 };
