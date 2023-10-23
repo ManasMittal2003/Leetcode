6bc7e473ef5471ffe1d9c397325d781a0db93fee
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-10-23 22:52:06 +0530

    Update 231. Power of Two.cpp

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int i=0;
-        while(1)
-        {
-            if(pow(2,i)>n){
-                return false;
-            }
-            else if(pow(2,i)==n){
-                return true;
-            }
-            i++;
+        if(n<=0){
+            return false;
         }
+        return (n&(n-1))==0;
     }
 };
