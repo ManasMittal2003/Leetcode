6ce15ea38d4ec9679752f1f1872f6667f3e30420
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-10-24 23:12:34 +0530

    Update 509. Fibonacci Number.cpp

--- 509. Fibonacci Number.cpp
+++ 509. Fibonacci Number.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
+    int fibo(int n,vector<int> &dp){
+        if(n<=1){
+            return dp[n];
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int last=fibo(n-1,dp);
+        int slast=fibo(n-2,dp);
+        dp[n]=last+slast;
+        return last+slast;
+    }
     int fib(int n) {
-        if(n==0)
+        if(n==0){
             return 0;
-        if(n==1)
-            return 1;
-        int ans=fib(n-1)+fib(n-2);
+        }
+        // if(n==1){
+        //     return 1;
+        // }
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        int ans=fibo(n,dp);
         return ans;
     }
 };
