31d96fd48fe2c846c411ccc771ffac70098a402b
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2023-10-30 19:35:17 +0530

    Update 50. Pow(x, n).cpp

--- 50. Pow(x, n).cpp
+++ 50. Pow(x, n).cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    double power(double x,int temp){
-        if(temp==0){
-            return 1;
-        }
-        double sp=power(x,temp-1);
-        double bp=x*sp;
-        return bp;
-    }
     double myPow(double x, int n) {
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        int temp=n;
-        if(n<0){
-            temp=(-1)*n;
+        if(n==0) return 1;
+        if(n<0) {
+            n = abs(n);
+            x = 1/x;
         }
-        double ans=power(x,temp);
-        if(n<0){
-            return 1.0/ans;
+        if(n%2==0){
+            return myPow(x*x, n/2);
         }
         else{
-            return ans;
+            return x*myPow(x, n-1);
         }
     }
 };
