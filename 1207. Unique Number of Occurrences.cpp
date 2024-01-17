4a1fabb3058700dc07a4b2255e9892fe0c42ba1c
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-01-17 12:45:31 +0530

    Update 1207. Unique Number of Occurrences.cpp

--- 1207. Unique Number of Occurrences.cpp
+++ 1207. Unique Number of Occurrences.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int> mp;
+        unordered_map<int,int> mapping;
         for(int i=0;i<arr.size();i++){
-            mp[arr[i]]++;
+            mapping[arr[i]]++;
         }
         vector<int> temp;
-        for(auto i:mp){
-            temp.push_back(i.second);
-        }
-        sort(temp.begin(),temp.end());
-        for(int i=0;i<temp.size()-1;i++){
-            if(temp[i]==temp[i+1]){
+        for(auto i:mapping){
+            auto it=find(temp.begin(),temp.end(),i.second);
+            if(it!=temp.end()){
                 return false;
+            }else{
+                temp.push_back(i.second);
             }
         }
         return true;
