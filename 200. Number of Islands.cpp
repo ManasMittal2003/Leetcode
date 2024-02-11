200ae72fe983f8f1dd95aba9307bc5b08b0549c8
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-02-11 15:02:17 +0530

    Update 200. Number of Islands.cpp

--- 200. Number of Islands.cpp
+++ 200. Number of Islands.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,int i,int j,vector<vector<int>> &visited){
-        queue<pair<int,int>> q;
+    void dfs(vector<vector<char>>& grid,int i,int j,vector<vector<int>> &visited){
+        visited[i][j]=1;
         int n=grid.size();
         int m=grid[0].size();
-        q.push(make_pair(i,j));
-        visited[i][j]=1;
-        while(!q.empty()){
-            pair<int,int> temp=q.front();
-            q.pop();
-            int row=temp.first;
-            int col=temp.second;
-            for(int i=-1;i<=1;i++){
-                if(i!=0){
-                    int newcol=col+i;
-                    if((newcol>=0&&newcol<m)&&(grid[row][newcol]=='1')&&(visited[row][newcol]==0)){
-                        visited[row][newcol]=1;
-                        bfs(grid,row,newcol,visited);
-                    }
-                }
+        int row=i;
+        int col=j;
+        for(int i=-1;i<=1;i++){
+            if(i==0) continue;
+            int newcol=col+i;
+            if((newcol>=0&&newcol<m)&&(grid[row][newcol]=='1')&&(visited[row][newcol]==0)){
+                dfs(grid,row,newcol,visited);
             }
-            for(int i=-1;i<=1;i++){
-                if(i!=0){
-                    int newrow=row+i;
-                    if((newrow>=0&&newrow<n)&&(grid[newrow][col]=='1')&&(visited[newrow][col]==0)){
-                        visited[newrow][col]=1;
-                        bfs(grid,newrow,col,visited);
-                    }
-                }
+        }
+        for(int i=-1;i<=1;i++){
+            if(i==0) continue;
+            int newrow=row+i;
+            if((newrow>=0&&newrow<n)&&(grid[newrow][col]=='1')&&(visited[newrow][col]==0)){
+                dfs(grid,newrow,col,visited);
             }
         }
     }
@@ -35,15 +25,15 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>> visited(n,vector<int>(m,0));
-        int cnt=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(visited[i][j]==0&&grid[i][j]=='1'){
-                    cnt++;
-                    bfs(grid,i,j,visited);
+        int islands=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'&&visited[i][j]==0){
+                    islands++;
+                    dfs(grid,i,j,visited);
                 }
             }
         }
-        return cnt;
+        return islands;
     }
 };
