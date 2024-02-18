5ffd138f767943e90d77694d3983bb5f49ecaa10
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-02-18 19:10:38 +0530

    Update 1335. Minimum Difficulty of a Job Schedule.cpp

--- 1335. Minimum Difficulty of a Job Schedule.cpp
+++ 1335. Minimum Difficulty of a Job Schedule.cpp
@@ -1,64 +1,45 @@
 class Solution {
 public:
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        return minDifficultyOptimized(jobDifficulty, d);
+    int t[301][11];
+    int solve(vector<int>& jobDifficulty, int n, int idx, int d) {
+        
+        // If you have only 1 day, then you will do all the remaining jobs
+        // and select the max difficulty as the answer
+        if (d == 1) {
+            return *max_element(begin(jobDifficulty) + idx, end(jobDifficulty));
+        }
+        
+        if (t[idx][d] != -1)
+            return t[idx][d];
+    
+        int Max = INT_MIN;
+        int result = INT_MAX;
+        
+        // Try one by one with all possibilities
+        /*
+            Take 1 job in one day
+            Take 2 jobs in one day
+            Take 3 jobs in one day
+            and so on
+            
+            Then find the optimal one among all the results
+        */
+        for (int i = idx; i <= n - d; i++) {
+            Max = max(Max, jobDifficulty[i]);
+            result = min(result, Max + solve(jobDifficulty, n, i + 1, d - 1));
+        }
+        
+        return t[idx][d] = result;
     }
-
-    int minDifficultyOptimized(const vector<int>& jobDifficulty, int d) {
-        const int n = static_cast<int>(jobDifficulty.size());
-
-        if (n < d) {
+    
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        int n = jobDifficulty.size();
+        
+        if (n < d)
             return -1;
-        } else if (n == d) {
-            int totalDifficulty = 0;
-            for (int difficulty : jobDifficulty)
-                totalDifficulty += difficulty;
-            return totalDifficulty;
-        }
 
-        // dp[j]:
-        //    the minimum difficulty OF
-        //       the first (j+1) jobs, exactly scheduled in (i+1) days.
-        vector<int> dp(n);
-        dp[0] = jobDifficulty[0];
-
-        // Initializing the dp array to store the maximum difficulty encountered so far.
-        for (int i = 1; i < n; ++i) {
-            dp[i] = max(jobDifficulty[i], dp[i - 1]);
-        }
-
-        vector<int> dpPrev(n);
-
-        // Use a decreasing stack to optimize performance.
-        vector<int> stkIdx;
-        stkIdx.reserve(n);
-        int lastIdx;
-
-        // Dynamic Programming to find the minimum difficulty (Optimized with a decreasing stack).
-        for (int i = 1; i < d; ++i) {
-            dp.swap(dpPrev);
-            stkIdx.clear();
-
-            for (int j = i; j < n; ++j) {
-                dp[j] = jobDifficulty[j] + dpPrev[j - 1];
-
-                // Process the decreasing stack.
-                while (!stkIdx.empty() && jobDifficulty[stkIdx.back()] <= jobDifficulty[j]) {
-                    lastIdx = stkIdx.back();
-                    stkIdx.pop_back();
-                    dp[j] = min(dp[j], dp[lastIdx] + jobDifficulty[j] - jobDifficulty[lastIdx]);
-                }
-
-                if (!stkIdx.empty()) {
-                    lastIdx = stkIdx.back();
-                    dp[j] = min(dp[j], dp[lastIdx]);
-                }
-
-                stkIdx.emplace_back(j);
-            }
-        }
-
-        return dp.back();
+        memset(t, -1, sizeof(t));
+        
+        return solve(jobDifficulty, n, 0, d);
     }
 };
-
