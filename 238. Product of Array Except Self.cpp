078353467e8d3a6ac4462f7c127bbbca5baf2871
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-03-15 06:27:26 +0530

    Update 238. Product of Array Except Self.cpp

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int left[nums.size()];
-        int right[nums.size()];
-        left[0]=1;
-        for(int i=1;i<nums.size();i++){
-            left[i]=left[i-1]*nums[i-1];
-        }
-        right[nums.size()-1]=1;
-        for(int i=nums.size()-2;i>=0;i--){
-            right[i]=right[i+1]*nums[i+1];
-        }
-        vector<int> ans;
+        vector<int> lpro(nums.size(),-1);
+        int pro=1;
         for(int i=0;i<nums.size();i++){
-            ans.push_back(left[i]*right[i]);
+            lpro[i]=pro;
+            pro=pro*nums[i];
         }
-        return ans;
+        pro=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            lpro[i]=lpro[i]*pro;
+            pro=pro*nums[i];
+        }
+        return lpro;
     }
 };
