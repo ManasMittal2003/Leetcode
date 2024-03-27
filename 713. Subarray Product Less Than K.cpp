16fcb8253536cf600166a8fb3fe0aeb23561014f
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-03-27 13:58:16 +0530

    Create 713. Subarray Product Less Than K.cpp

---
+++ 713. Subarray Product Less Than K.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1){
+            return 0;
+        }
+        int pro=1;
+        int i=0,j=0;
+        int cnt=0;
+        while(j<nums.size()){
+            pro=pro*nums[j];
+            while(pro>=k){
+                pro=pro/nums[i];
+                i++;
+            }
+            cnt+=j-i+1;
+            j++;
+        }
+        return cnt;
+    }
+};
