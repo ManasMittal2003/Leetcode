b3e84bf75591fc15e29e7b332fd82140be5daa23
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-04-04 17:44:00 +0530

    Update 126. Word Ladder II.cpp

--- 126. Word Ladder II.cpp
+++ 126. Word Ladder II.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
-    void dfs(string end,string &begin,map<string,int> &mp,vector<vector<string>> &ans,vector<string> sample){
-        if(end==begin){
+    void solve(string end, string& begin, vector<vector<string>>& ans,
+               vector<string> sample, unordered_map<string, int>& mp) {
+        if (end == begin) {
             sample.push_back(end);
-            reverse(sample.begin(),sample.end());
+            reverse(sample.begin(), sample.end());
             ans.push_back(sample);
             return;
         }
         sample.push_back(end);
-        for(int i=0;i<end.length();i++){
-            char org=end[i];
-            string temp=end;
-            for(char ch='a';ch<='z';ch++){
-                end[i]=ch;
-                if((mp.find(end)!=mp.end())&&(mp[end]+1==mp[temp])){
-                    dfs(end,begin,mp,ans,sample);
+        string temp = end;
+        for (int i = 0; i < end.length(); i++) {
+            char org = end[i];
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                end[i] = ch;
+                if (mp.find(end) != mp.end() && mp[end] == mp[temp] - 1) {
+                    solve(end, begin, ans, sample, mp);
                 }
             }
-            end[i]=org;
+            end[i] = org;
         }
     }
-    vector<vector<string>> findLadders(string begin, string end, vector<string>& word) {
-        vector<vector<string>> ans;
-        queue<pair<string,int>> q;
-        map<string,int> mp;
-        set<string> st;
-        st.insert(word.begin(),word.end());
-        q.push({begin,0});
+    vector<vector<string>> findLadders(string begin, string end,
+                                       vector<string>& word) {
+        unordered_set<string> st;
+        st.insert(word.begin(), word.end());
+        unordered_map<string, int> mp;
+        queue<pair<string, int>> q;
+        q.push({begin, 1});
         st.erase(begin);
-        while(!q.empty()){
-            string temp=q.front().first;
-            int lvl=q.front().second;
+        while (!q.empty()) {
+            string temp = q.front().first;
+            int lvl = q.front().second;
+            mp[temp] = lvl;
             q.pop();
-            mp[temp]=lvl;
-            for(int i=0;i<temp.length();i++){
-                char org=temp[i];
-                for(char ch='a';ch<='z';ch++){
-                    temp[i]=ch;
-                    if(st.find(temp)!=st.end()){
+            for (int i = 0; i < temp.length(); i++) {
+                char org = temp[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    temp[i] = ch;
+                    if (st.find(temp) != st.end()) {
+                        q.push({temp, lvl + 1});
                         st.erase(temp);
-                        q.push({temp,lvl+1});
                     }
                 }
-                temp[i]=org;
+                temp[i] = org;
             }
         }
+        vector<vector<string>> ans;
         vector<string> sample;
-        dfs(end,begin,mp,ans,sample);
+        solve(end, begin, ans, sample, mp);
         return ans;
     }
 };
