c469cabf4fdd793c54f4fa4126466d2f70a6b9fc
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-04-05 09:32:55 +0530

    Create 1544. Make The String Great.cpp

---
+++ 1544. Make The String Great.cpp
@@ -0,0 +1,23 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stk;
+        for (char ch : s) {
+            if (!stk.empty() && abs(stk.top() - ch) == 32) {
+                stk.pop();
+            } else {
+                stk.push(ch);
+            }
+        }
+        string result = "";
+        while (!stk.empty()) {
+            result = stk.top() + result;
+            stk.pop();
+        }
+        return result;
+    }
+};
