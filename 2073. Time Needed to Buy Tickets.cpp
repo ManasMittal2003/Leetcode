7688c9089ceb6e307540c70135a51956780f3a39
17ManasMittal <140882469+17ManasMittal@users.noreply.github.com>
2024-04-10 01:56:09 +0530

    Update 2073. Time Needed to Buy Tickets.cpp

--- 2073. Time Needed to Buy Tickets.cpp
+++ 2073. Time Needed to Buy Tickets.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        // queue<pair<int,int>> q;
-        // for(int i=0;i<tickets.size();i++){
-        //     q.push({i,tickets[i]});
-        // }
-        // int cnt=0;
-        // while(!q.empty()){
-        //     int ticket=q.front().second;
-        //     int ind=q.front().first;
-        //     q.pop();
-        //     ticket--;
-        //     cnt++;
-        //     if(ind==k&&ticket==0){
-        //         return cnt;
-        //     }
-        //     else if(ticket!=0){
-        //         q.push({ind,ticket});
-        //     }
-        // }
-        // return 0;
-
-        int i=0;
+        queue<pair<int,int>> q;
+        for(int i=0;i<tickets.size();i++){
+            q.push({i,tickets[i]});
+        }
         int cnt=0;
-        while(tickets[k]!=0){
-            if(tickets[i]!=0){
-                tickets[i]--;
-                cnt++;
+        while(!q.empty()){
+            int ticket=q.front().second;
+            int ind=q.front().first;
+            q.pop();
+            ticket--;
+            cnt++;
+            if(ind==k&&ticket==0){
+                return cnt;
             }
-            i++;
-            if(i==tickets.size()){
-                i=0;
+            else if(ticket!=0){
+                q.push({ind,ticket});
             }
         }
-        return cnt;
+        return 0;
+    }
+};
 
-        
+//O(n) Solution
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=0;
+        for(int i=0;i<tickets.size();i++){
+            if(i<=k){
+                ans+=min(tickets[i],tickets[k]);
+            }else{
+                ans+=min(tickets[k]-1,tickets[i]);
+            }
+        }
+        return ans;
     }
 };
