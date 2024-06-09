6a17718c91b2d0d9b732cfa0e9be6726c2144e3c
ManasMittal2003 <140882469+ManasMittal2003@users.noreply.github.com>
2024-06-09 11:06:49 +0530

    Create 974. Subarray Sums Divisible by K.cpp

---
+++ 974. Subarray Sums Divisible by K.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0]++;
+        int sum = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum = sum + nums[i];
+            int rem = sum % k;
+            if ((rem) < 0) {
+                rem += k;
+            }
+            if (mp.find(rem) != mp.end()) {
+                ans += mp[rem];
+            }
+            mp[rem]++;
+        }
+        return ans;
+    }
+};
