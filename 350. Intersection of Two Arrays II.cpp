5400cdf55c4fa89d7fb166d8b215cde733fa60f3
ManasMittal2003 <140882469+ManasMittal2003@users.noreply.github.com>
2024-07-02 20:18:00 +0530

    Update 350. Intersection of Two Arrays II.cpp

--- 350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        vector<int> res;
-        int i=0,j=0;
-        while(i<nums1.size()&&j<nums2.size()){
-            if(nums1[i]==nums2[j]){
-                res.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j]){
-                i++;
-            }
-            else{
-                j++;
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums1.size(); i++) {
+            mp[nums1[i]]++;
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            if (mp.find(nums2[i]) != mp.end()) {
+                ans.push_back(nums2[i]);
+                mp[nums2[i]]--;
+                if (mp[nums2[i]] == 0) {
+                    mp.erase(nums2[i]);
+                }
             }
         }
-        return res;
+        return ans;
     }
 };
