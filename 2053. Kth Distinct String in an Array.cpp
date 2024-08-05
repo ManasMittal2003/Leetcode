cbb6852d004b3d37d9d6431449315b8c7abe1a52
ManasMittal2003 <140882469+ManasMittal2003@users.noreply.github.com>
2024-08-05 12:59:06 +0530

    Update 2053. Kth Distinct String in an Array.cpp

--- 2053. Kth Distinct String in an Array.cpp
+++ 2053. Kth Distinct String in an Array.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        map<string, int> mapping;
-        for (auto i : arr) {
-            mapping[i]++;
+        unordered_map<string, int> mp;
+        for (int i = 0; i < arr.size(); i++) {
+            mp[arr[i]]++;
         }
-
-        string ans = "";
-        for (auto i : arr) {
-            if (mapping[i] == 1) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (mp[arr[i]] == 1) {
                 k--;
                 if (k == 0) {
-                    ans = i;
-                    break;
+                    return arr[i];
                 }
             }
         }
-
-        return ans;
+        return "";
     }
 };
